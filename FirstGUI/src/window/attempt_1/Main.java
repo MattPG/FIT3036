d9d3b3e2d7ccd0aedd339f2c08a8ee5928d16292
Matthew Greenwood <mattpgreenwood@gmail.com>
2014-07-29 17:45:56 +1000

    Made a windowframe

---
+++ FirstGUI/src/window/attempt_1/Main.java
@@ -0,0 +1,20 @@
+package window.attempt_1;
+
+import javax.swing.JFrame;
+
+public class Main {
+		
+	@SuppressWarnings("serial")
+	public static class MyFrame extends JFrame{
+		public MyFrame(){
+			setTitle("My Empty Frame");
+			setSize(300,200); // default size is 0,0
+			setLocation(10,200); // default is 0,0 (top left corner)
+		}
+	}
+
+	  public static void main(String[] args) {
+	    JFrame f = new MyFrame();
+	    f.setVisible(true);
+	  }
+}
