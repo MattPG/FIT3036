f0a43b2867fb3c1a6cae320c79615ded6d59bc37
Matthew Greenwood <mattpgreenwood@gmail.com>
2014-10-20 00:55:08 +1100

    - Fixed up unused imports

--- CableGateProgram/src/cablegate/Main.java
+++ CableGateProgram/src/cablegate/Main.java
@@ -1,38 +1,17 @@
 package cablegate;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.SortedTermVectorMapper;
-import org.apache.lucene.index.TermVectorEntry;
 import org.datafx.controller.flow.Flow;
 import org.datafx.controller.flow.FlowHandler;
-import org.hibernate.search.FullTextSession;
-import org.hibernate.search.Search;
-import org.hibernate.search.SearchFactory;
-import org.mcavallo.opencloud.Cloud;
-import org.mcavallo.opencloud.Cloud.Case;
-import org.mcavallo.opencloud.Tag;
 import org.slf4j.LoggerFactory;
 
 import cablegate.infrastructure.DatabaseManager;
 import cablegate.infrastructure.SystemConfig;
-import cablegate.models.Cable;
-import cablegate.table.TableController;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
